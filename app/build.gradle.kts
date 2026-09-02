fe81d05ea7af0e42c28a340dd1ae11e61f98ea83
sandipchawda1991-dotcom <sandipchawda1991@gmail.com>
2026-09-02 23:47:07 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,5 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation("com.google.api-client:google-api-client-android:2.2.0") {
-        exclude(group = "org.apache.httpcomponents")
-    }
-    implementation("com.google.apis:google-api-services-drive:v3-rev20220815-2.0.0") {
-        exclude(group = "org.apache.httpcomponents")
-    }
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
